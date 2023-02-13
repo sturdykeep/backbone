c5c11de4e3dca009937552fbe8b271d252760ef7
DevOwl <christian.muehle@devowl.de>
2023-02-13 21:04:31 +0100

    Update comments

--- lib/prelude/sprite/mod.dart
+++ lib/prelude/sprite/mod.dart
@@ -2,7 +2,7 @@ import 'package:backbone/builders.dart';
 import 'package:backbone/prelude/sprite/system.dart';
 import 'package:backbone/prelude/sprite/trait.dart';
 
-/// Add the spritetrait and spriteSystem
+/// Add the spritetrait and spriteSystem to your relam
 void spritePlugin(RealmBuilder builder) {
   builder.withTrait(SpriteTrait);
   builder.withSystem(spriteSystem);
