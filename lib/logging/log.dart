04f9f4c12aa6d4e523688afae22030fc3ec36c2b
DevOwl <christian.muehle@devowl.de>
2023-02-25 14:07:58 +0100

    Change type for time

--- lib/logging/log.dart
+++ lib/logging/log.dart
@@ -1,6 +1,6 @@
 abstract class Log {
   void startTrace(String trace);
   void endTrace(String trace);
-  void addSample(String key, double milliseconds);
+  void addSample(String key, int milliseconds);
   void addEvent(String event, {String? payload});
 }
